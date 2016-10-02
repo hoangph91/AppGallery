b58037c1968a99acb8ae924e8b03757588aec6e6
hoangph <hoanguet1991@gmail.com>
2016-10-02 15:31:16 +0700

    Day la lan up thu 4

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'hoang.nghetinh.org/Gallery';
+$config['base_url'] = 'hoang.nghetinh.org/Gallery/Gallery';
 
 /*
 |--------------------------------------------------------------------------
